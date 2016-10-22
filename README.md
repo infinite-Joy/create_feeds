c42f0820432fc78720ab399949f9755720dc3c16
Joydeep BHattacharjee <joydeepubuntu@gmail.com>
2016-10-22 08:21:00 +0000

    added the ep3

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # create_feeds
 To generate feeds for playerfm and possibly other platforms
+
+# to get the item
+
+cat rssfile | tr "\n" "|" | grep -o '<item>.*</item>' | sed 's/\(<item>\|<\/item>\)//g' | sed 's/|/\n/g'
+
+
+add item and change the last build date
--- gen_feed.py
+++ gen_feed.py
@@ -18,31 +18,24 @@ def main():
     fg.link( href='https://www.flawcode.com', rel='alternate' )
     fg.logo('https://pbs.twimg.com/profile_images/774831574177067009/tuxOiOZj.jpg')
     fg.subtitle(' The other side story ')
-    fg.link( href='https://www.flawcode.com/atom.rss', rel='self' )
+    fg.link( href='https://www.flawcode.com', rel='self' )
     fg.language('en')
 
     fg.podcast.itunes_category('Technology', 'Podcasting')
-    fe = fg.add_entry()
-    fe.id('https://www.flawcode.com/episode/show/1/ep01_deep_learning_with_vijay.mp3')
-    fe.title('Deep Learning with Vijay')
-    fe.author( {'name':'FlawCode','email':'0xflawcode@gmail.com'} )
-    fe.link( href='https://www.flawcode.com/episode/show/1/index.html', rel='alternate' )
-    fe.description(get_description("ep01_description.txt"))
-    fe.enclosure('https://www.flawcode.com/episode/show/1/ep01_deep_learning_with_vijay.mp3', 0, 'audio/mpeg')
 
     fe = fg.add_entry()
-    fe.id('https://www.flawcode.com/episode/show/2/ep02_venture_capitalism_with_abishek_and_debanjoli.mp3')
-    fe.title('Venture Capitalism With Abishek and Debanjoli')
+    fe.id('https://flawcode.com/episode/show/3/ep03_julia_with_viral.mp3')
+    fe.title('Julia with Viral')
     fe.author( {'name':'FlawCode','email':'0xflawcode@gmail.com'} )
-    fe.description(get_description("ep02_description.txt"))
-    fe.link( href='https://flawcode.com/index.html', rel='alternate' )
-    fe.enclosure('https://www.flawcode.com/episode/show/2/ep02_venture_capitalism_with_abishek_and_debanjoli.mp3', 0, 'audio/mpeg')
+    fe.description(get_description("ep03_description.txt"))
+    fe.link( href='https://flawcode.com/episode/show/3/', rel='alternate' )
+    fe.enclosure('https://flawcode.com/episode/show/3/ep03_julia_with_viral.mp3', 0, 'audio/mpeg')
 
 
 
 
     fg.rss_str(pretty=True)
-    fg.rss_file('atom_episode2_003.rss')
+    fg.rss_file('atom_episode3.rss')
 
 
 if __name__ == "__main__":
