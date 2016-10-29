c160de0e5290b57fa87850ba56c21ae2ec2b5337
Joydeep BHattacharjee <joydeepubuntu@gmail.com>
2016-10-29 14:11:12 +0000

    ep 4

--- gen_feed.py
+++ gen_feed.py
@@ -24,18 +24,18 @@ def main():
     fg.podcast.itunes_category('Technology', 'Podcasting')
 
     fe = fg.add_entry()
-    fe.id('https://flawcode.com/episode/show/3/ep03_julia_with_viral.mp3')
-    fe.title('Julia with Viral')
+    fe.id('https://flawcode.com/episode/show/4/ep04_APL_with_Morten.mp3')
+    fe.title('APL with Morten')
     fe.author( {'name':'FlawCode','email':'0xflawcode@gmail.com'} )
-    fe.description(get_description("ep03_description.txt"))
-    fe.link( href='https://flawcode.com/episode/show/3/', rel='alternate' )
-    fe.enclosure('https://flawcode.com/episode/show/3/ep03_julia_with_viral.mp3', 0, 'audio/mpeg')
+    fe.description(get_description("ep04_description.txt"))
+    fe.link( href='https://flawcode.com/episode/show/4/', rel='alternate' )
+    fe.enclosure('https://flawcode.com/episode/show/4/ep04_APL_with_Morten.mp3', 0, 'audio/mpeg')
 
 
 
 
     fg.rss_str(pretty=True)
-    fg.rss_file('atom_episode3.rss')
+    fg.rss_file('atom_episode4.rss')
 
 
 if __name__ == "__main__":
